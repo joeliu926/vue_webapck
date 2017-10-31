eb3045d5a1069c9fd555388fe0762836089a7199
liuzhao <liuzhao@rongcapital.cn>
2017-10-31 18:06:36 +0800

    1806

---
+++ src/modules/customerInfo/components/member/index.js
@@ -0,0 +1,21 @@
+export default {
+    components: {},
+    data () {
+        return {
+
+        }
+    },
+    created() {
+        this.searchData();
+    },
+    mounted(){
+
+    },
+    destroyed() {
+
+    },
+    methods: {
+
+
+    }
+}
---
+++ src/modules/customerInfo/components/top/index.vue
@@ -0,0 +1,7 @@
+<template src="./template.html"></template>
+<script src="./index.js">
+
+</script>
+<style  lang="scss">
+
+</style>
