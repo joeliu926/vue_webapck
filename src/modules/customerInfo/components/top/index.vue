0f8a96ef2626215d87b40b881d88ae06f5119913
yongchao71 <yongchao71@163.com>
2017-11-01 09:58:07 +0800

    add customer top

--- src/modules/customerInfo/components/top/index.vue
+++ src/modules/customerInfo/components/top/index.vue
@@ -4,4 +4,30 @@
 </script>
 <style  lang="scss">
 
+.customerinfo-top-content{
+background-color:#534CE7;
+    width: 100%;
+    clear: both;
+    border: none;
+    height: 134px;
+    .customerinfo-outer{
+        margin: 20px auto;
+        padding: 24px;
+background-color: transparent;
+        height: 156px;
+        .customerinfo-inner{
+            border-radius: 4px;
+            background-color:#ffffff;
+        }
+        .customertop-img{
+            padding: 24px;
+        }
+        .customer-name{
+            font-weight: bold;
+        }
+    }
+    .border-padding{
+        padding-top: 24px;
+    }
+}
 </style>
