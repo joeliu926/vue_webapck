b4148578d2c838c1d47b0d8b199fcaaa31c38b41
liuzhao <liuzhao@rongcapital.cn>
2017-11-01 09:55:23 +0800

    0955

--- src/modules/customerInfo/index.vue
+++ src/modules/customerInfo/index.vue
@@ -3,5 +3,14 @@
 
 </script>
 <style  lang="scss">
+.customer-info{
+    .customers-content{margin: 24px;
+    .el-tabs__header{
+        background-color:#fff;
+        .is-active{border-top: 3px solid #8162e0 !important; color:#333333; font-weight: bold;}
+        .el-tabs__item{border-top: 3px solid #fff;}
+    }
 
+    }
+}
 </style>
