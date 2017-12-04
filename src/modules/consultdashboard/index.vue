50f7c6f8503c38508f65222b5d6dfc3e6e7d6984
yongchao71 <yongchao71@163.com>
2017-12-04 11:12:57 +0800

    add consult dashboard

--- src/modules/consultdashboard/index.vue
+++ src/modules/consultdashboard/index.vue
@@ -14,10 +14,57 @@
             .waiting-ct{
                 width: 100%;
                 .waiting-handle{
-padding: 12px 0;
+                     padding: 12px 0;
+                    .add-consult{
+                        text-align: right;
+                    }
+                    .today-count{
+                        font-size: 14px;
+                        color:#999999;
+                        padding: 0 0 12px 0 ;
+                        .p-count{
+                            font-size: 16px;
+                            color:#333333;
+                            margin-left: 30px;
+                        }
+                    }
+                    .des{
+                        padding: 0px;
+                    }
                 }
-                .add-consult{
-                    text-align: right;
+
+                .customer-table{
+                    color: #999999;
+                    width:100%;
+                    border: 1px solid #eaeaea;
+                    th{
+                        background-color: #F7F7F7;
+                        padding: 10px 24px;
+                        font-size: 16px;
+                        color: #333333;
+                    }
+                        tr{
+                            width: 100%;
+                        }
+                        td{
+                            padding: 10px 24px;
+                            font-size: 14px;
+                        }
+                        .day-ct{
+                            border-top:1px solid #eaeaea;
+                            padding: 10px 24px;
+                            font-size: 14px;
+                            width: 100%;
+                            .idate{
+
+                            }
+                            .icount{
+                                margin-left: 24px;
+                                color:#666666;
+                            }
+                        }
+
+
                 }
             }
         }
