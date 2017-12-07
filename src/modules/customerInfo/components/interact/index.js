430b4d3156b696de1e63c405849ca64d2c9f4f17
liuzhao <liuzhao@rongcapital.cn>
2017-12-07 17:21:01 +0800

    1720

--- src/modules/customerInfo/components/interact/index.js
+++ src/modules/customerInfo/components/interact/index.js
@@ -2,11 +2,12 @@ export default {
     components: {},
     data () {
         return {
-
+            selectedVal:'',
+            culeList:[]
         }
     },
     created() {
- 
+        this.changeClue();
     },
     mounted(){
 
@@ -19,6 +20,26 @@ export default {
             var anchor = this.$el.querySelector(selector);
             document.body.scrollTop = anchor.offsetTop; // chrome
             document.documentElement.scrollTop = anchor.offsetTop; // firefox
+        },
+        changeClue(){
+            let _this=this;
+            let uid= _this.$route.params.id;
+            let postData = {
+                status:1,
+                customerId:uid
+            };
+            _.ajax({
+                url: '/customers/culelist',
+                method: 'POST',
+                data: postData,
+                success: function (result) {
+                    if (result.code == 0 && result.data) {
+                        _this.culeList = result.data;
+                        console.log('_this.culeList',_this.culeList);
+                    }else {
+                    }
+                }
+            }, 'withCredentials');
         }
 
     }
