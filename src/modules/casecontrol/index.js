d4d52b96e645ad4f845753a197041ac7c9c238ef
yongchao71 <yongchao71@163.com>
2017-12-11 19:58:32 +0800

    change style

--- src/modules/casecontrol/index.js
+++ src/modules/casecontrol/index.js
@@ -26,6 +26,7 @@ export default {
             oCurrentShowItemIndex: 0,//当前展示的案例的索引
             oSourceList:[],//客户来源列表
             isFillProject:false,//结束是否填写项目信息
+            isAllowNameDrop:false, //是否允许根据名称模糊查询下拉
             oNameList:[],
             otheritems: "",
             otherresion: "",
@@ -81,9 +82,12 @@ export default {
                 _This.consultItems.push(item.projectCode);
             });
         }
+  /*      if(_This.routerParam.adddiag){
+            _This.isAllowNameDrop=true;
+        }*/
         _This.initSocket();
         _This.fGetCustomerData();
-         console.log("this.$route.params--------->",this.$route.params);
+         //console.log("this.$route.params--------->",this.$route.params);
         _This.fProductList();
         _This.fDoctorList();
         _This.fUpdateClue();
@@ -395,7 +399,7 @@ export default {
          * @returns {boolean}
          */
         fChangeAutoSelect(ename){
-           // console.log("--------自动选择-----------",ename);
+           console.log("--------自动选择-- project---------",ename);
             if (ename.trim() == "") {
                 return false;
             }
@@ -421,7 +425,7 @@ export default {
          * 下拉框选中
          */
         fSelectProjecChange(eCode){
-           // console.log("----下拉结束选中----",eCode);
+            console.log("----下拉结束选中 project----",eCode);
             let _This = this;
             let routerParam = _This.routerParam;
             _This.routerParam.projects = _This.routerParam.projects || [];
@@ -569,39 +573,19 @@ export default {
         fGetCustomerList(ename){
             console.log("-=-=-=-=进入模糊-=-=-=-",ename,this.oCustomer.name,"000000-=-=-=-");
             var _This = this;
-            this.oCustomer.name=ename;
-            if(_This.oCustomer.name==""){
+
+           // this.oCustomer.name=ename;
+            if(_This.oCustomer.name==""||ename==""){
                 return false;
             }
-            let namel=_This.oCustomer.name.indexOf("(");
-            if(namel>0){
-                /// console.log("-----rrrrrrrrrr-------》",namel);
-                _This.oCustomer.name=_This.oCustomer.name.substr(0,namel);
-            }
-            var postData={
-                startDate:"",
-                endDate:"",
-                pageNo: 1,
-                pageSize: 6,
-                fieldValue:_This.oCustomer.name,
-                searchField:"name"
-            };
-            _.ajax({
-                url: '/customers/customerlist',
-                method: 'POST',
-                data: postData,
-                success: function (result) {
-                    console.log("模糊搜索客户列表-------》",result);
-                    if(result.code==0&&result.data){
-                       _This.oNameList=result.data.list;
-                        if(!_This.oCustomer.id&&_This.routerParam.adddiag&&result.data.list.length==1){
-                            _This.oCustomer=_This.oNameList[0];
-                        }
-                    }
+            _This.fSearchUserDpData(ename,function (result) {
+                if(result.code==0&&result.data){
+                    _This.oNameList=result.data.list;
+                    _This.oCustomer.name=ename;
 
-
+                    console.log("_This.oNameList---->",_This.oNameList);
                 }
-            }, 'withCredentials');
+            });
             //console.log("22222_This.oCustomer-=-=-=-=-",_This.oCustomer);
         },
         /**
@@ -609,7 +593,46 @@ export default {
          */
         fSelectNameItem(ename){
             console.log("-=-=-=-=-=-choose=-=rrrr-=-=-=-=-=-=-",ename);
-            //this.oCustomer.name=ename;
+            let _This=this;
+            if(!_This.routerParam.adddiag){
+                return false;
+            }
+            _This.fSearchUserDpData(ename,function (result) {
+                if(result.code==0&&result.data){
+                    if(_This.routerParam.adddiag&&result.data.list.length==1&&result.data.list[0].name== _This.oCustomer.name){
+                        _This.oCustomer=result.data.list[0];
+                     }
+                }
+            });
+        },
+        fSearchUserDpData(ename,callback){
+            var postData={
+                startDate:"",
+                endDate:"",
+                pageNo: 1,
+                pageSize: 6,
+                fieldValue:ename,
+                searchField:"name"
+            };
+            _.ajax({
+                url: '/customers/customerlist',
+                method: 'POST',
+                data: postData,
+                success: function (result) {
+                    callback(result);
+                    console.log("模糊搜索客户列表-------》",result);
+                    //if(result.code==0&&result.data){
+                      //  _This.oNameList=result.data.list;
+                      //  if(result.data.list.length==0){
+
+                      //  }
+                     //   _This.oCustomer.name=ename;
+                        /*  if(!_This.oCustomer.id&&_This.routerParam.adddiag&&result.data.list.length==1){
+                         _This.oCustomer=_This.oNameList[0];
+                         }*/
+                   // }
+                }
+            }, 'withCredentials');
         },
         /**
          * 获取面诊客户渠道来源
@@ -722,6 +745,9 @@ export default {
          */
         fUpdateClue(){
             let _This = this;
+            if(!_This.routerParam.appointmentId){
+                return false;
+            }
             let postData = {
                 appointmentId:  _This.routerParam.appointmentId,
                 phase: 2,
