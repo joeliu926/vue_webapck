d4039e8cc02f26ebc54efc7fcbf9923809e6d6ef
yiweidujiang <yiweidujiang869527@163.com>
2017-12-12 13:05:04 +0800

    分诊样式的调整

--- src/modules/triage/index.vue
+++ src/modules/triage/index.vue
@@ -2,25 +2,34 @@
 <script src="./index.js">
 
 </script>
-<style lang="scss">
+<style scope lang="scss">
 
 
 .triage-content{
     margin: 24px;
     .el-tabs__header{
-        background-color:#fff;
-        .is-active{border-top: 3px solid #8162e0 !important; color:#333333; font-weight: bold;}
-        .el-tabs__item{border-top: 3px solid #fff;}
-    }
+            background-color:#fff;
+            .el-tabs__item{   
+                 border-top: 3px solid transparent;
+                 border-bottom: 1px solid transparent;
+             }
+            .el-tabs__item.is-active{
+                border-top: 3px solid #8162e0 !important; 
+                border-bottom:1px solid #FFF;
+                color:#333333;
+                font-weight: bold;
+            }
+
+        }
+    // .el-tabs--border-card> .el-tabs__content {
+    //     padding-top: 10px;
+    // }
     .content-list{
         .content-left-border{
             border-radius:4px;
         }
     }
 
-    .el-tabs--border-card>.el-tabs__content {
-        padding-top: 10px;
-    }
     .customer-block-content {
         margin :2px;
         border: 1px solid transparent;
