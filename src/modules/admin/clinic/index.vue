d70cf04b2cdd4795f8097da562b05e1c0e344cd1
liuzhao <liuzhao@rongcapital.cn>
2017-12-21 12:58:00 +0800

    1256

--- src/modules/admin/clinic/index.vue
+++ src/modules/admin/clinic/index.vue
@@ -5,7 +5,7 @@
 
 <style lang="scss">
     .admin-clinic {
-        .admin-rbox{height: 100%; width: 82%;display: inline-block;box-sizing: border-box; padding:12px; vertical-align: top;}
-        .admin-lbox{height: 100%; width:18%; display: inline-block;padding:12px;box-sizing: border-box;}
+        .admin-rbox{height: 100%; width: 88%;display: inline-block;box-sizing: border-box; padding:12px; vertical-align: top;}
+        .admin-lbox{height: 100%; width:12%; display: inline-block;padding:12px;box-sizing: border-box;}
     }
 </style>
--- src/modules/admin/tree/index.js
+++ src/modules/admin/tree/index.js
@@ -5,62 +5,181 @@
 
 export default {
     components: {},
-        data() {
-            return {
-                data4: [{
-                    id: 1,
-                    label: '一级 1',
-                    children: [{
-                        id: 4,
-                        label: '二级 1-1',
-                        children: [{
-                            id: 9,
-                            label: '三级 1-1-1'
-                        }, {
-                            id: 10,
-                            label: '三级 1-1-2'
-                        }]
-                    }]
+    data() {
+        return {
+            checkedKeys:[1,2],
+            treeData:
+            [
+                {
+                    id:'admin_admin',
+                    label: '管理',
+                    linkUrl:'',
+                    hightline:false,
+                    needinit:false,
+                    color:'#3542f1'
+
+                },
+            {
+                id:'admin_user',
+                label: '用户',
+                linkUrl:'',
+                hightline:false,
+                children: [{
+                    id:'admin_user_user',
+                    label: '用户',
+                    linkUrl:'',
+                    hightline:true,
+                    needinit:true
+
+                },{
+                    id:'admin_user_role',
+                    label: '角色',
+                    linkUrl:'',
+                    hightline:false,
+                    needinit:false
+
+                }]
+            },
+            {
+                id:'admin_clinic',
+                label: '诊所',
+                linkUrl:'',
+                hightline:false,
+                needinit:false,
+                children: [{
+                    id:'admin_clinic_info',
+                    label: '诊所信息',
+                    linkUrl:'',
+                    hightline:false,
+                    needinit:false
+
                 }, {
-                    id: 2,
-                    label: '一级 2',
-                    children: [{
-                        id: 5,
-                        label: '二级 2-1'
-                    }, {
-                        id: 6,
-                        label: '二级 2-2'
-                    }]
+                    id:'admin_clinic_prod',
+                    label: '项目',
+                    linkUrl:'',
+                    hightline:false,
+                    needinit:false
+
                 }, {
-                    id: 3,
-                    label: '一级 3',
-                    children: [{
-                        id: 7,
-                        label: '二级 3-1'
-                    }, {
-                        id: 8,
-                        label: '二级 3-2'
-                    }]
-                }],
-                defaultProps: {
-                    children: 'children',
-                    label: 'label'
-                }
-            }
+                    id:'admin_clinic_doctor',
+                    label: '医生',
+                    linkUrl:'',
+                    hightline:false,
+                    needinit:false
+
+                }, {
+                    id:'admin_clinic_case',
+                    label: '案例',
+                    linkUrl:'',
+                    hightline:false,
+                    needinit:false
+                }]
+            },
+            {
+                id:'admin_environ',
+                label: '环境',
+                linkUrl:'',
+                hightline:false,
+                needinit:false,
+                color:'#3542f1'
+            },
+            {
+                id:'admin_log',
+                label: '日志',
+                linkUrl:'',
+                hightline:false,
+                needinit:false,
+                children: [{
+                    id:'admin_log_login',
+                    label: '登陆日志',
+                    linkUrl:'',
+                    hightline:false,
+                    needinit:false
+
+                }, {
+                    id:'admin_log_operate',
+                    label: '操作日志',
+                    linkUrl:'',
+                    hightline:false,
+                    needinit:false
+
+                }]
+            }]
+        };
     },
     created() {
 
     },
     mounted(){
-
+        //document.querySelector('#admin_user_role').parentNode.className += ' el-tree-hight';
     },
     destroyed() {
 
     },
     methods: {
+        handleNodeClick(data) {
+            this.removeAllHightLine();
+            let tickPoint =data.id.split('_');
+            if(tickPoint.length>2){
+                data.hightline =true;
+            }
+        },
+        renderContent(h, { node, data, store }) {
+            if(node.data.hightline){
+                this.setHightline(node.data.id)
+            }else{
+                this.removeHightline(node.data.id)
+            }
+            if(node.data.color){
+                return (
+                    <span id={data.id} on-click={ () => this.handleNodeClick(data) } style="align-items: center; justify-content: space-between; width:100%;font-size: 14px; padding-right: 8px;color:#3542f1;">
+                    <span>
+                    <span>{node.label}</span>
+                </span>
+                </span>
+                );
 
-    },
-    watch: {
+            }else{
+                if(node.data.needinit)
+                {
+                    return (
+                        <span id={data.id} on-click={ () => this.handleNodeClick(data) } style="align-items: center; justify-content: space-between;width:100%; font-size: 14px; padding-right: 8px;">
+                            <span>
+                                <span>{node.label}</span>
+                            </span>
+                            <span  style="color:red; font-size:14px; padding-left:10px;">●</span>
+                        </span>
+                    );
+                }else{
+                    return (
+                    <span id={data.id} on-click={ () => this.handleNodeClick(data) } style="align-items: center; justify-content: space-between; width:100%;font-size: 14px; padding-right: 8px;">
+                        <span>
+                            <span>{node.label}</span>
+                        </span>
+                    </span>
+                    );
+                }
+            }
 
+        },
+        setHightline(params){
+            this.$nextTick(function () {
+                document.querySelector('#'+params).parentNode.className = 'el-tree-node__content el-tree-hight';
+            })
+
+        },
+        removeHightline(params){
+            this.$nextTick(function () {
+                document.querySelector('#'+params).parentNode.className = 'el-tree-node__content';
+            })
+        },
+        removeAllHightLine(params){
+            this.treeData.forEach(m=>{
+               m.hightline = false;
+               m.children&&m.children.forEach(ms=>{
+                   ms.hightline = false;
+               })
+            });
+        }
     }
 }
