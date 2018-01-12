adda7beec0a5f6e9a557d2ed0e7255f8628dc01a
liuzhao <liuzhao@rongcapital.cn>
2018-01-12 09:58:57 +0800

    0837

--- src/common/utils/constants.js
+++ src/common/utils/constants.js
@@ -5,6 +5,6 @@ module.exports ={
     pageSize: 15,
     host:"https://nihaomc.com/node_test", //"http://localhost:8023",  https://27478500.qcloud.la/node
     fileDownload:"",
-    fileUpload:"https://nihaomc.com/uploadimg/",
+    fileUpload:"https://nihaomc.com/uploadimg_test/",
     wsReqUrl:"ws://140.143.184.52:8054/",//wss://nihaomc.com/websocket/",wss://27478500.qcloud.la/websocket/"
 };
