679329ca72d76ea7d1bea7036e405a189ac57fda
yongchao71 <yongchao71@163.com>
2018-01-18 11:41:16 +0800

    add post

--- src/modules/admin/post/index.js
+++ src/modules/admin/post/index.js
@@ -3,7 +3,7 @@
  */
 
 import tree from '../tree/index.vue';
-
+import CONSTANT from '../../../common/utils/constants.js';
 export default {
     components: {
         tree
@@ -11,6 +11,15 @@ export default {
     data () {
         let defaultl=require("../../../common/img/login_logo.png");
         return {
+            defaultImg: require("../../../common/img/add-img-icon.png"), //默认上传图片
+            imgUploadUrl:CONSTANT.fileUpload+"attachment/upload",//上传图片地址
+            addPost:false,//新增海报标志
+            addPostcate:false, //新增海报分类状态
+            eidtPostcate:false, //编辑海报分类状态
+            currentEdit:0, //当前的编辑item
+            aPostClassify:[{id:1,name:"分类一"},{id:2,name:"分类二"},{id:3,name:"分类三"}],//海报分类
+            selectClassify:2,//选中的分类
+            uploadPostImg:""//当前上传的图片
         };
     },
     filters:{
@@ -40,9 +49,132 @@ export default {
                 }
             }, 'withCredentials');
         },
-        fCreateMap(){
-        	 // var map = new BMap.Map("map-content");
+        /**
+         * 新增海报
+         */
+        fAddPost(){
+            let _This = this;
+            console.log("add post----------");
+            _This.addPost=true;
+
+
         },
+        /**
+         * 关闭新增海报
+         */
+        fCloseAddPost(){
+            let _This = this;
+            console.log("add post----------");
+            _This.addPost=false;
+        },
+        /**
+         * 选择海报分类
+         */
+        fSelectClassify(cmd){
+           console.log("select classsify------>");
+        },
+        /**
+         * 选择海报图片
+         */
+        fChooseImg() {
+            this.$refs.uploadImg.click();
+        },
+        /**
+         * 异步文件上传
+         */
+        fAjaxFileUpload(e) {
+            let _This = this;
+            var imgFile = e.target.files[0];
+
+            let aLogoType=[".jpg",".jpeg",".png",".bmp"];
+            let imgName=imgFile.name.substr(imgFile.name.lastIndexOf(".")).toLocaleLowerCase();
+            if(aLogoType.indexOf(imgName)<0){
+                _This.$message.error("上传图片格式错误");
+                return false;
+            }
+            if(imgFile.size > 5*1024*1024) {
+                _This.$message.error("图片大小不能超过5M");
+                return false;
+            }
+            var fdata = new FormData();
+            fdata.append('imgFile', imgFile);
+            fdata.append('user', "test");
+            _.ajax({
+                url: _This.imgUploadUrl,
+                type: 'POST',
+                data: fdata,
+                urlType: 'full',
+                contentType: false,
+                processData: false,
+                success: function(result) {
+                    console.log("upload result-----",result);
+                    if(result.code == 0 && result.data.length > 0) {
+                       _This.uploadPostImg=result.data[0];
+                    }
+                },
+                error: function(result) {
+                    console.log("error-- result------>", result)
+                }
+            });
+        },
+        /**
+         * 新增海报分类
+         */
+        fAddPostClassify(){
+            let _This = this;
+            console.log("add post----------");
+            _This.addPostcate=true;
+        },
+        /**
+         * 确认新增
+         */
+        fAddCommit(){
+            let _This = this;
+            _This.addPostcate=false;
+            console.log("add sure----------")
+        },
+        /**
+         * 删除海报
+         */
+        fDelPost(item){
+            let _This=this;
+            console.log("delete post------>",item);
+        },
+        /**
+         * 关闭新增
+         */
+        fCloseAdd(){
+            let _This = this;
+            _This.addPostcate=false;
+            console.log("add cancel----------")
+        },
+        /**
+         * 选择海报分类
+         * @param classid
+         */
+        fSelectClassify(classid){
+            let _This = this;
+            console.log("select classify-----",classid)
+        },
+        /**
+         * 编辑海报
+         */
+        fEditPost(item){
+            let _This=this;
+            _This.currentEdit=item;
+        },
+        /**
+         * 编辑确认提交
+         */
+        fEditCommit(item,editorcancel){
+            let _This=this;
+            if(!editorcancel){
+                _This.currentEdit=0;
+                return false;
+            }
+            _This.currentEdit=item;
+            console.log("edit classify-----",item)
+        }
     },
     watch: {}
 }
--- src/modules/admin/post/index.vue
+++ src/modules/admin/post/index.vue
@@ -16,6 +16,9 @@
             font-size: 16px;
             color: #333333;
         }
+        .cur{
+            cursor:pointer;
+        }
  .i-block{
      display: inline-block;
      box-sizing: border-box;
@@ -57,22 +60,56 @@
                        width: 80px;
                        color: #333333;
                    }
-                    .s-item-ct{
+                    .select-item{
+                        color: #9083ED;
+                    }
+                    .s-item-ct{ /**********顶部选择版式***********/
                         .post-classify{
-                            margin:0 24px 0 0;
-                          cursor: pointer;
+                            margin:0 10px 0 0;
+                            cursor:pointer;
+                            .add-cate{
+                                vertical-align: middle;
+                                margin-bottom: 4px;
+                            }
+                            .sure-btn{
+                                width: 24px;
+                              padding: 0 4px;
+                            }
+                            .sure-btn:hover{
+                                color: #9083Ed;
+                            }
+                            .twidth{
+                                width: 100px;
+                            }
                         }
-
+                        .classify-hover:hover{
+                            color: #9083Ed;
+                        }
+                        .edit-icon{
+                            visibility: hidden;
+                            width: 20px;
+                            padding-left: 4px;
+                        }
+                        .classify-hover:hover .edit-icon{
+                            visibility: visible;
+                        }
+                        .add-post-des{
+                            padding: 100px 0 0 0;
+                            height: 300px;
+                            box-sizing: border-box;
+                            border-bottom: 1px solid #dddddd;
+                        }
                     }
                     .f-choice{
                         cursor: pointer;
                     }
-                    .format-item{
+                    .format-item{ /************图片模板版式**************/
                         width: 300px;
                         height:400px;
                         vertical-align: top;
                         position: relative;
                         margin-right: 24px;
+                        border: 1px solid #9083ED;
                         .post-pic{
                             width: 100%;
                         }
@@ -81,18 +118,83 @@
                             bottom: 0;
                             position: absolute;
                             box-sizing: border-box;
-                            vertical-align: top;
+
                             padding: 12px;
+                            height: 100px;
                             .des-logo{
                                 width: 48%;
-                                vertical-align: top;
+                                vertical-align: middle;
                                 text-align: center;
+                                .img-logo{
+                                    height: 60px;
+                                    width: 60px;
+                                }
+                            }
+                        }
+                        .close-icon{
+                            position: absolute;
+                            right:-10px;
+                            top:-10px;
+                            cursor: pointer;
+                            .img{
+                                height: 20px;
+                                width: 20px;
                             }
                         }
                     }
                 }
             }
 
+        }
+        .mask{
+            position: fixed;
+            top:0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: rgba(0,0,0,0.5);
+            z-index: 1000;
+            .mask-ct{
+                width: 500px;
+                height: 400px;
+                position: absolute;
+                top:50%;
+                left: 50%;
+                margin-top: -200px;
+                margin-left: -250px;
+                background: #ffffff;
+                border-radius: 4px;
+                .end-close{
+                    width: 30px;
+                    height: 30px;
+                    position: absolute;
+                    right: -25px;
+                    top:-20px;
+                    cursor: pointer;
+                }
+                .select-ct{
+                  text-align: center;
+                    padding: 50px 0;
+                }
+                .select-img-ct{
+                    text-align: center;
+                    .post-img{
+                        display: inline-block;
+                        width: 140px;
+                        height: 140px;
+                        border: 1px dashed #bfcbd9;
+                        border-radius: 6px;
+                        cursor: pointer;
+                        background-position: center center;
+                        background-repeat: no-repeat;
+                        background-size: 100%;
+                    }
+                }
+                .submit-ct{
+                   text-align: center;
+                    padding: 24px;
+                }
+            }
         }
     }
 </style>
